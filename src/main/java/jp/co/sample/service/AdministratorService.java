81d4719d76aa5910722b643652b366f0656648e0
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-06-17 13:39:24 +0900

    servce追加

---
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -0,0 +1,31 @@
+package jp.co.sample.service;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.domain.Administrator;
+import jp.co.sample.repository.AdministratorRepository;
+
+@Service
+@Transactional
+public class AdministratorService {
+
+	@Autowired
+	private AdministratorRepository addAdministratorRepository;
+	
+	
+	public void insert(Administrator administrator) {
+		addAdministratorRepository.insert(administrator);
+	}
+	
+	public List<Administrator> findByMailAddressAndPassword(String mailAdress, String password){
+		return addAdministratorRepository.findByMailAddressAndPassword(mailAdress, password);
+	}
+	
+	
+	
+}
