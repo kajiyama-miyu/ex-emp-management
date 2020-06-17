4d12e2337dc652c188e800cd77506a152e786860
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-06-17 14:04:58 +0900

    controller変更

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -2,9 +2,11 @@ package jp.co.sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdiministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -18,5 +20,28 @@ public class AdministratorController {
 	@ModelAttribute
 	public InsertAdiministratorForm setUpInsertAdministratorForm() {
 		return new InsertAdiministratorForm();
+	}
+	
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator/insert";
+	}
+	/**
+	 * 
+	 * 管理者情報の登録。
+	 * @param insertAdiministratorForm
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdiministratorForm insertAdiministratorForm, Model model) {
+		Administrator administrator = new Administrator();
+		administrator.setName(insertAdiministratorForm.getName());
+		administrator.setMailAddress(insertAdiministratorForm.getMailAddress());
+		administrator.setPassword(insertAdiministratorForm.getPassword());
+		
+		administratorService.insert(administrator);
+		
+		return "redirect://";
 	}
 }
