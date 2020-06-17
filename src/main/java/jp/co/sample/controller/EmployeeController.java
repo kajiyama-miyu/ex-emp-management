979d130c94ab6651f4cf06f120952eca9b4c3718
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-06-17 22:17:44 +0900

    Controller変更

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -1,5 +1,24 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employee")
 public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	
+	@RequestMapping("/showList")
+	public String showList(Model model) {
+		employeeService.showList();
+		return "";
+	}
 
 }
